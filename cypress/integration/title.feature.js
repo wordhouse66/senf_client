f13d23a9f0119b087d8f760b488d1a2468208453
Thomas <thomas@craftacademy.se>
2021-08-08 21:27:43 +0200

    updates the test message

--- cypress/integration/title.feature.js
+++ cypress/integration/title.feature.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 describe("When Accessing the Application", () => {
-  it("should have a page title of Senf.koeln", () => {
+  it("is expected to have a page title set to Senf.koeln", () => {
     cy.visit("/");
     cy.title().should("include", "Senf.koeln");
   });
