61261462ffde8f57a00f5baaf3b431fc6b4ba39d
„tassilomorino“ <tassilomorino@gmail.com>
2021-08-25 13:41:53 +0200

    created translation directory and split up translations files into JSONs

--- src/App.js
+++ src/App.js
@@ -42,6 +42,8 @@ import Cookies from "universal-cookie";
 
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import translationEN from "./util/translations/english.json";
+import translationDE from "./util/translations/german.json";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -51,16 +53,10 @@ i18n
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
     resources: {
       en: {
-        translation: {
-          rotate_tablet: "Please rotate your tablet",
-          rotate_phone: "Please rotate your smartphone",
-        },
+        translation: translationEN,
       },
       de: {
-        translation: {
-          rotate_tablet: "Bitte rotiere dein Tablet",
-          rotate_phone: "Bitte rotiere dein Smartphone",
-        },
+        translation: translationDE,
       },
     },
     lng: "de", // if you're using a language detector, do not define the lng option
